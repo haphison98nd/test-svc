8c784820f66144e8bded49f0bf172e2d3afd97ab
Hà Phi Sơn <35570775+haphison98nd@users.noreply.github.com>
2019-11-11 02:41:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,137 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            error: '',
+            isLoaded: false,
+            response: [],
+            value: '',
+            isShowButton: false,
+            currentPage : 1,
+        }
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleCheck = this.handleCheck.bind(this);
+    }
+
+
+    handleChange(event){
+        this.setState({
+            value : event.target.value,
+            currentPage : 1,
+            isShowButton: false,
+            response: [],
+        });
+    }
+
+    handleSubmit() {
+        
+        fetch("https://api.github.com/users/" + this.state.value + "/repos" + "?page=" + this.state.currentPage)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    // console.log(result);
+                    // console.log(this.state.response);
+                    this.setState({
+                        isLoaded: true,
+                        response: this.state.response.concat(result),
+                        currentPage: this.state.currentPage+1,
+                    });
+                    this.handleCheck();
+                    // console.log(this.state.response);
+                },
+                
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+    handleCheck(){
+        // let elmButton= null;
+        // this.setState({currentPage:this.state.currentPage+1});
+        fetch("https://api.github.com/users/" + this.state.value + "/repos" + "?page=" + this.state.currentPage)
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    isShowButton: result.length !==0 
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+        )
+    }
+
+    render() {
+        // let response = this.state.response;
+        let { error, isLoaded, response } = this.state;
+        let elmButton, elmError, emlLoading, elmCount, elmList = null;
+
+        elmCount = <div> {this.state.response.length} </div>;
+        
+        if (this.state.isShowButton == true) {
+            elmButton = <button onClick={this.handleSubmit} type="button" className="btn btn-success showmore"> Show More </button>;
+        }
+        // console.log(this.state.isShowButton);
+        if (error) {
+            // elmError =  <div> Error: ko tim thay user </div>;
+            // elmList = '';
+            console.log(error.message);
+        }
+        else if (!isLoaded) {
+            elmButton = '';
+            elmList = '';
+        }
+        else{
+            elmList = (
+                <ul className="list-repo">
+                {response.map(item => (
+                    <li key={item.name}>
+                        {item.full_name}
+                    </li>
+                ))}
+
+                </ul> 
+            )
+        }
+        return (
+            <div className="container wrapper">
+                <div className="form-inpt">
+                    <form>
+                        <label>
+                            Name:
+                            <input className="username" type="text" value={this.state.value} onChange={this.handleChange} />
+                        </label>
+                        <input className="btn-submit btn-success" type="button" onClick={this.handleSubmit} value="Submit" />
+                    </form>
+
+                </div>
+                {elmError}
+                <div>
+                    {elmList}
+                    
+                </div>
+                {emlLoading}
+                {elmButton}
+
+            </div>
+        ); 
+    }
+}
 export default App;
